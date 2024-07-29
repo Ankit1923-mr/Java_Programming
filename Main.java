9f57a759f6d41586933b011ee3d04d0de6a5e488
Ankit1923-mr <145849699+Ankit1923-mr@users.noreply.github.com>
2024-07-29 21:32:19 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String args[]) {
+        System.out.println("Hello World");
+        String name = "Ankit Kumar";
+        System.out.println(name);
+        final int a = 15;
+        System.out.println(a);
+        // a = 20; // This line will cause a compilation error because 'a' is final
+        // System.out.println(a);
+    }
+}
+
