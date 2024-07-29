0c870de8ae57ca329caa4b1cca494caf5f21ab82
Ankit1923-mr <145849699+Ankit1923-mr@users.noreply.github.com>
2024-07-29 21:59:40 +0530

    Add files via upload

---
+++ Autocast.java
@@ -0,0 +1,8 @@
+public class Autocast {
+    public static void main(String args[]) {
+        int num = 7;
+        double doublenum = num; // automatic casting
+        System.out.println(num);
+        System.out.println(doublenum);
+    }
+}
