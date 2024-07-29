9c69ed1d8509b01dd49e1414d32764a0e6799ae2
Ankit1923-mr <145849699+Ankit1923-mr@users.noreply.github.com>
2024-07-29 22:02:02 +0530

    Add files via upload

---
+++ Operator.java
@@ -0,0 +1,11 @@
+public class Operator {
+    public static void main(String args[]) {
+        int n = 5;
+        String a = "oh";
+        // int sum = n + a; // This line will cause a compilation error
+        // System.out.println(sum);
+        String c = a + n; // adding in string format
+        System.out.println(c);
+    }
+}
+
